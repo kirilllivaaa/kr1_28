6529b776df6113427e602e05f36745cb65fc7006
PolinaPonomar <ponomareva.polia2013@yandex.ru>
2018-12-28 12:24:48 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+   std::cout << "Hello, World!" << std::endl;
+   int a =2;
+   a*=3;
+   std::cout << a << std::endl;
+   a=-6;
+   std :: cout << a << std::endl;
+
+    return 0;
+}
